5bd2af7efaed71c726bdd9dd5e601fb8e178b6a5
carson <carsonjamescook@gmail.com>
2016-09-05 20:48:37 -0700

    Added the new ItemModelTest.

--- lists/tests.py
+++ lists/tests.py
@@ -2,6 +2,7 @@ from django.template.loader import render_to_string
 from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
+from lists.models import Item
 
 # Get rid of csrf_token for comparison -----------------------------------------
 import re
@@ -49,3 +50,17 @@ class HomePageTest(TestCase):
         # ----------------------------------------------------------------------
 
         self.assertEqual(observed_html, expected_html)
+
+class ItemModelTest(TestCase):
+
+    def test_saving_and_retrieving_items(self):
+        first_item = Item()
+        first_item.text = 'The first (ever) list item'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Item the second'
+        second_item.save()
+
+        saved_items = Item.objects.all()
+        self.assertEqual(saved_items.count(), 2)s
